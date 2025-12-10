205424ae921eb73bdd33cbdc2f7dcd05d015c5e5
Yash Borkar <yashborkar@Yashs-MacBook-Pro.local>
2025-12-11 02:12:49 +0530

    Dark mode

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 console.log("Money Master")
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleBtn = document.getElementById('theme-toggle');
+    const body = document.body;
+    const icon = toggleBtn.querySelector('i');
+
+    // Check for saved user preference, if any, on load of the website
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        }
+    }
+
+    toggleBtn.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        let theme = 'light';
+        if (body.classList.contains('dark-mode')) {
+            theme = 'dark-mode';
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+        } else {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+        }
+        localStorage.setItem('theme', theme);
+    });
+});
+
